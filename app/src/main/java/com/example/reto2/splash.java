dc2e6d569546d6356650198a2d57269554ef65b1
jobaniCanon <89047481+jobaniCanon@users.noreply.github.com>
2021-12-12 21:12:29 -0500

    reto 2

---
+++ app/src/main/java/com/example/reto2/splash.java
@@ -0,0 +1,26 @@
+package com.example.reto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    private final int Time=5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },Time);
+    }
+}
